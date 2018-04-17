9168e9be742cbea174dd1a7e4e4d8a9293ab8567
Tony Thigpen <tonythigpen@outlook.com>
2018-04-17 07:53:18 -0400

    Modified models.

--- Warudo/admin.py
+++ Warudo/admin.py
@@ -3,12 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from Warudo import models
 
-
-@admin.register(models.LocalCity, models.LocalState)
-class LocationAdmin(admin.ModelAdmin):
-    pass
-
-
 admin.site.register(models.Profile)
 admin.site.register(models.Cosplayer)
 admin.site.register(models.Convention)
--- Warudo/models.py
+++ Warudo/models.py
@@ -1,30 +1,7 @@
 from django.db import models
 
 from Accounts.models import User
-
-
-class LocalCity(models.Model):
-    city = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.city
-
-
-class LocalState(models.Model):
-    state = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.state
-
-
-class Convention(models.Model):
-    name = models.CharField(max_length=200)
-    city = models.ForeignKey(LocalCity, models.CASCADE, related_name="ConventionCity")
-    state = models.ForeignKey(LocalState, models.CASCADE, related_name="ConventionState")
-    competition = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
+from Gamma import settings
 
 
 class Cosplayer(models.Model):
@@ -35,29 +12,63 @@ class Cosplayer(models.Model):
         ('P', 'Professional'),
     )
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    Identity = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='assets/img')
-    city = models.ForeignKey(LocalCity, models.CASCADE, related_name="CosplayerCity")
-    state = models.ForeignKey(LocalState, models.CASCADE, related_name="CosplayerState")
-    lvl = models.CharField(max_length=20, choices=C_LEVEL)
-    compete = models.ManyToManyField(Convention)
+    EXPERTISE = (
+        ('Needlework', 'Needle'),
+        ('Armoring', 'Armor'),
+        ('Makeup', 'MaUp'),
+        ('Large Build', 'BigBuild'),
+    )
+
+    cosplayer_id = models.IntegerField(primary_key=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    identity = models.CharField(max_length=100, null=True)
+    photo = models.ImageField(upload_to='assets/img', null=True)
+    focus = models.CharField(max_length=15, choices=EXPERTISE, null=True)
+    lvl = models.CharField(max_length=20, choices=C_LEVEL, null=True)
+    city = models.CharField(max_length=20, null=True)
+    state = models.CharField(max_length=20, null=True)
 
     def __str__(self):
-        return self.Identity
+        return self.identity
+
+
+class Address(models.Model):
+    address_id = models.IntegerField(primary_key=True)
+    addressLine1 = models.CharField(max_length=140)
+    addressLine2 = models.CharField(max_length=140, blank=True, null=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=2)
+    zip = models.IntegerField
+
+
+class Contest(models.Model):
+    contest_id = models.IntegerField(primary_key=True)
+    contest_name: models.CharField(max_length=200)
+    placement1st: models.ForeignKey(Cosplayer, models.CASCADE, null=True)
+    placement2nd: models.ForeignKey(Cosplayer, models.CASCADE, null=True)
+    placement3rd: models.ForeignKey(Cosplayer, models.CASCADE, null=True)
+
+
+class Convention(models.Model):
+    convention_id = models.AutoField(primary_key=True)
+    manager_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=200)
+    competition = models.ForeignKey(Contest, models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, models.CASCADE)
-    cosplayer = models.OneToOneField(Cosplayer, models.CASCADE, primary_key=False)
-    conventions = models.ManyToManyField(Convention)
+    profile_id = models.IntegerField(primary_key=True, auto_created=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    cosplayer = models.OneToOneField(Cosplayer, on_delete=models.CASCADE, blank=True)
+    conventions = models.ManyToManyField(Convention, blank=True)
 
     @models.permalink
     def get_absolute_url(self):
         return 'profile', (), {'pk': self.pk}
-
-    def cos_location(self):
-        return "{}, {}".format(self.cosplayer.city, self.cosplayer.state)
 
     def __str__(self):
         return self.user.username
